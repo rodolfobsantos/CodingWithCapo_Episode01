c942e21b651673120786a46c21c40c2598822728
Rodolfo Santos <rodolfobsantos@gmail.com>
2023-11-26 22:29:43 -0300

    adding text to speech

--- chatgpt_client.py
+++ chatgpt_client.py
@@ -1,25 +1,35 @@
-import os
-import openai
+from openai import OpenAI
+from gtts import gTTS
 from dotenv import load_dotenv
 
 load_dotenv()
 
-openai.api_key = os.getenv("OPENAI_KEY")
+client = OpenAI()
 
 question = input("Qual a sua pergunta para o ChatGPT? ")
 
-response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
-        {"role": "system", "content": "You are a chatbot"},
-        {"role": "user", "content": f"{question}"},
-    ]
-)
+def ask_question(question):
+    response = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a chatbot"},
+            {"role": "user", "content": f"{question}"},
+        ]
+    )
 
-result = ''
-for choice in response.choices:
-    result += choice.message.content
+    result = ''
+    for choice in response.choices:
+        result += choice.message.content
 
-print(f"Perguntamos ao chatGPT: {question}")
-print(f"Aqui está a resposta dele: ")
-print(result)
+    print(f"Perguntamos ao chatGPT: {question}")
+    print(f"Aqui está a resposta dele: ")
+    return result
+
+def create_mp3(result):
+    language = 'en'
+    mp3 = gTTS(text = f"{result}", lang=language)
+    mp3.save(f'{question}.mp3')
+
+answer = ask_question(question)
+print(answer)
+create_mp3(answer)
